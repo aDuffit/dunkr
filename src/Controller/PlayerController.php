7b418cdfee0e041c07677e3806fec0efc7b83876
Aymeric Duffit <aymeric.duffit@circet.fr>
2026-01-19 15:52:13 +0100

    création d'une bdd de dev

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -7,7 +7,6 @@ use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\UX\Chartjs\Builder\ChartBuilder;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
 
